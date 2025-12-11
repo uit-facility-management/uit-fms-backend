6d6381f9a1b6a0c0c34a6e4b053463c3cced8c7b
KhoaBH <lamanhkhoa2004@gmail.com>
2025-12-11 15:17:01 +0700

    feat: add transaction and validation data before insert into database

--- src/schedule/schedule.service.ts
+++ src/schedule/schedule.service.ts
@@ -1,22 +1,45 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateScheduleDto } from './dto/create-schedule.dto';
 import {
   UpdateScheduleDto,
   UpdateScheduleStatusDto,
 } from './dto/update-schedule.dto';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Schedule, ScheduleStatus } from './entities/schedule.entity';
+import { Schedule } from './entities/schedule.entity';
 import { Repository } from 'typeorm';
+import { DataSource } from 'typeorm';
 @Injectable()
 export class ScheduleService {
   constructor(
     @InjectRepository(Schedule)
     private readonly scheduleRepository: Repository<Schedule>,
+    private readonly dataSource: DataSource,
   ) {}
   async create(createScheduleDto: CreateScheduleDto) {
-    const schedule = this.scheduleRepository.create(createScheduleDto);
-    return this.scheduleRepository.save(schedule);
+    return await this.dataSource.transaction(async (manager) => {
+      const { room_id, start_time, end_time, period_start, period_end } =
+        createScheduleDto;
+      const conflict = await manager
+        .createQueryBuilder('schedule', 's')
+        .setLock('pessimistic_write')
+        .where('s.room_id = :roomId', { roomId: room_id })
+        .andWhere('s.start_time < :end', { end: end_time })
+        .andWhere('s.end_time > :start', { start: start_time })
+        .andWhere('s.period_start < :pEnd', { pEnd: period_end })
+        .andWhere('s.period_end > :pStart', { pStart: period_start })
+        .getOne();
+      if (conflict) {
+        throw new ConflictException('Phòng đã có lịch trùng. Không tạo được.');
+      }
+      const schedule = manager.create(Schedule, createScheduleDto);
+      return await manager.save(schedule);
+    });
   }
+
   async updateStatus(id: string, status: UpdateScheduleStatusDto) {
     const schedule = await this.scheduleRepository.findOne({ where: { id } });
     if (schedule) {
