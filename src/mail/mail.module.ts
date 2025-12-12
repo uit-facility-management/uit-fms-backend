1e8e41dc809fcff10ec9b6eb5133876131231f52
KhoaBH <lamanhkhoa2004@gmail.com>
2025-12-12 21:53:53 +0700

    refactor: refactor redis config

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,5 +1,6 @@
-import { Controller, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { BullModule } from '@nestjs/bullmq';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MailService } from './mail.service';
 import { MailProcessor } from './mail.processor';
 import { MailController } from './mail.controller';
@@ -7,15 +8,35 @@ import { UserModule } from 'src/user/user.module';
 
 @Module({
   imports: [
-    BullModule.forRoot({
-      connection: {
-        host: 'localhost',
-        port: 6379,
+    ConfigModule.forRoot({ isGlobal: true }),
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        const redisUrl = config.get<string>('REDIS_URL');
+
+        if (redisUrl) {
+          return {
+            connection: {
+              url: redisUrl,
+            },
+          };
+        }
+
+        return {
+          connection: {
+            host: 'localhost',
+            port: 6379,
+          },
+        };
       },
     }),
+
     BullModule.registerQueue({
       name: 'mail_queue',
-    }),UserModule
+    }),
+
+    UserModule,
   ],
   controllers: [MailController],
   providers: [MailService, MailProcessor],
