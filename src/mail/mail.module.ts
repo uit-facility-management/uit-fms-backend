d9df9dedf6eb39a05cf9d4c5e35d8014f15fd9b8
KhoaBH <lamanhkhoa2004@gmail.com>
2025-12-13 11:46:11 +0700

    revert: revert to redis

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,14 +1,43 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { BullModule } from '@nestjs/bullmq';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MailService } from './mail.service';
+import { MailProcessor } from './mail.processor';
 import { MailController } from './mail.controller';
 import { UserModule } from 'src/user/user.module';
+import { RedisOptions } from 'ioredis';
 
 @Module({
-  imports: [ConfigModule, UserModule],
+  imports: [
+    ConfigModule,
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        connection: {
+          url: config.get<string>('REDIS_URL'),
+          tls: {},
+        } as RedisOptions,
+      }),
+    }),
+
+    BullModule.registerQueue({
+      name: 'mail_queue',
+      defaultJobOptions: {
+        removeOnComplete: true,
+        removeOnFail: true,
+        backoff: {
+          type: 'fixed',
+          delay: 30000,
+        },
+      },
+    }),
+
+    UserModule,
+  ],
 
   controllers: [MailController],
-  providers: [MailService],
+  providers: [MailService, MailProcessor],
   exports: [MailService],
 })
 export class MailModule {}
