31dab0b85bf5e4f49450e2f7c929598f0c41c0a9
KhoaBH <lamanhkhoa2004@gmail.com>
2025-12-19 11:13:44 +0700

    feat: nodemailer

--- src/mail/mail.queue.service.ts
+++ src/mail/mail.queue.service.ts
@@ -19,8 +19,8 @@ export class MailQueueService implements OnModuleInit, OnModuleDestroy {
     // 1. Config Nodemailer
     this.transporter = nodemailer.createTransport({
       host: 'smtp.gmail.com',
-      port: 465,
-      secure: true,
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.GMAIL_USER,
         pass: process.env.GMAIL_APP_PASSWORD,
