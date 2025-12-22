376adc2c6494079aaf8b7ee96ca9bbe3ca70ed8d
KhoaBH <lamanhkhoa2004@gmail.com>
2025-12-22 22:23:03 +0700

    fix: add status to update device dto

--- src/device/dto/update-device.dto.ts
+++ src/device/dto/update-device.dto.ts
@@ -1,4 +1,13 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateDeviceDto } from './create-device.dto';
+import { DeviceStatus } from '../entities/device.entity';
+import { IsEnum } from 'class-validator';
 
-export class UpdateDeviceDto extends PartialType(CreateDeviceDto) {}
+export class UpdateDeviceDto extends PartialType(CreateDeviceDto) {
+  @ApiProperty({
+    example: 'ACTIVE',
+    description: 'Current status of the device',
+  })
+  @IsEnum(DeviceStatus)
+  status?: DeviceStatus;
+}
