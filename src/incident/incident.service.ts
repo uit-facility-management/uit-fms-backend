13a5918b1dc268c3faf64bf791c6d20ff956a5b6
KhoaBH <lamanhkhoa2004@gmail.com>
2025-12-23 14:26:02 +0700

    fix: update status

--- src/incident/incident.service.ts
+++ src/incident/incident.service.ts
@@ -4,11 +4,17 @@ import { UpdateIncidentDto } from './dto/update-incident.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Incident, IncidentStatus } from './entities/incident.entity';
 import { In, Repository } from 'typeorm';
-import { RoomAsset, RoomAssetStatus } from 'src/room-assets/entities/room-asset.entity';
+import {
+  RoomAsset,
+  RoomAssetStatus,
+} from 'src/room-assets/entities/room-asset.entity';
 import { RoomAssetsService } from 'src/room-assets/room-assets.service';
 
 @Injectable()
 export class IncidentService {
+  private readonly logger = new (require('@nestjs/common').Logger)(
+    IncidentService.name,
+  );
   constructor(
     @InjectRepository(Incident)
     private incidentRepository: Repository<Incident>,
@@ -16,16 +22,22 @@ export class IncidentService {
   ) {}
   async create(createIncidentDto: CreateIncidentDto) {
     const incident = this.incidentRepository.create(createIncidentDto);
-    this.roomAssetsService.updateStatus(createIncidentDto.room_asset_id, RoomAssetStatus.INACTIVE);
+    this.roomAssetsService.updateStatus(
+      createIncidentDto.room_asset_id,
+      RoomAssetStatus.INACTIVE,
+    );
     return this.incidentRepository.save(incident);
   }
 
   async findByRoom(room_id: string) {
-    return this.incidentRepository.find({ where: { room_asset: { room: { id: room_id } } },relations: ['room_asset','room_asset.room', 'created_user'] });
+    return this.incidentRepository.find({
+      where: { room_asset: { room: { id: room_id } } },
+      relations: ['room_asset', 'room_asset.room', 'created_user'],
+    });
   }
   async findAll() {
     return this.incidentRepository.find({
-      relations: ['room_asset','room_asset.room', 'created_user'],
+      relations: ['room_asset', 'room_asset.room', 'created_user'],
     });
   }
 
@@ -38,13 +50,23 @@ export class IncidentService {
   }
 
   async updateStatus(id: string, status: IncidentStatus) {
-    const incident = await this.incidentRepository.findOneBy({ id });
+    this.logger.log(`Updating room asset status to ACTIVE:${id}`);
+    const incident = await this.incidentRepository.findOne({
+      where: { id },
+      relations: ['room_asset'],
+    });
     if (!incident) {
       throw new NotFoundException(`Incident with id ${id} not found`);
     }
-    const room_asset = await this.roomAssetsService.findOne(incident.room_asset.id);
-    if (status === IncidentStatus.RESOLVED && room_asset) {
-      await this.roomAssetsService.updateStatus(room_asset.id, RoomAssetStatus.ACTIVE);
+    this.logger.log(
+      'Updating room asset status to ACTIVE:',
+      incident.room_asset.id,
+    );
+    if (status === IncidentStatus.RESOLVED ) {
+      await this.roomAssetsService.updateStatus(
+        incident.room_asset.id,
+        RoomAssetStatus.ACTIVE,
+      );
     }
     return this.incidentRepository.save(incident);
   }
