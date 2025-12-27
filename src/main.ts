d28047d67c6f4f4f2ed42d02b6687f5935ce8d46
KhoaBH <lamanhkhoa2004@gmail.com>
2025-12-27 13:06:39 +0700

    feat: config cors

--- src/main.ts
+++ src/main.ts
@@ -14,9 +14,18 @@ async function bootstrap() {
     .build();
 
   app.enableCors({
-    origin:
-      'http://localhost:3000, https://uit-fms-frontend-production.up.railway.app',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        'http://localhost:3000',
+        'https://uit-fms-frontend-production.up.railway.app',
+      ];
+
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true,
   });
 
